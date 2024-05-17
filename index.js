d1771466b62d88fd74e9f425ea7cfa8c27480995
SaqibTahir0201 <sqbmaaz@gmail.com>
2024-05-17 09:15:25 +0500

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+let typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Front-end Web Developer",
+    "Back-end Web Developer",
+    "Full-stack Web Developer",
+    "Web Designer",
+    "Mobile Web Developer",
+    "Web Application Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
